b6b16657cf675d942ee163d22c4005d85df26f0c
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2018-09-03 15:44:48 -0500

    added box and file size

--- ts.c
+++ ts.c
@@ -6,31 +6,36 @@
 #include <string.h>
 #include <time.h>
 #include <curses.h>
+#include <ncurses.h>
+#include <sys/stat.h>
 
-
-int sort_array(int c,char tosort[c][256]){
-	char sorted[c][256]; char temp[256];
-	for(int i=0; i<c; i++)
+int sort_array(int c, char tosort[c][256])
+{
+	char sorted[c][256];
+	char temp[256];
+	for (int i = 0; i < c; i++)
 	{
 		//printf("%d: %s\n",i,tosort[i]);
-		strcpy(sorted[i],tosort[i]);
+		strcpy(sorted[i], tosort[i]);
 	}
 
-	for(int i=0; i<c-1; i++){
-		for(int j=i+1; j<c; j++){
-			if(strcmp(sorted[i],sorted[j])>0)
+	for (int i = 0; i < c - 1; i++)
+	{
+		for (int j = i + 1; j < c; j++)
+		{
+			if (strcmp(sorted[i], sorted[j]) > 0)
 			{
 				strcpy(temp, sorted[i]);
-                strcpy(sorted[i], sorted[j]);
-                strcpy(sorted[j], temp);
+				strcpy(sorted[i], sorted[j]);
+				strcpy(sorted[j], temp);
 			}
 		}
 	}
 
-	printf("\t(%d Directory: %s)\n",0,sorted[0]);
-	for(int i=1; i<c; i++)
+	printf("\t(%d Directory: %s)\n", 0, sorted[0]);
+	for (int i = 1; i < c; i++)
 	{
-		printf("\t\t\t(%d Directory: %s)\n",i,sorted[i]);
+		printf("\t\t\t(%d Directory: %s)\n", i, sorted[i]);
 	}
 	return 0;
 }
@@ -40,27 +45,47 @@ int main(void)
 	pid_t child;
 	DIR *d;
 	struct dirent *de;
+	struct stat statbuffer;
 	int i, c, k;
 	char s[356], cmd[256];
 	time_t t;
 
+	initscr();
+	WINDOW *win = newwin(50, 150, 1, 1); //create window
+
 	while (1)
 	{
-		getchar();
+		werase(win);
+		refresh();
+		box(win, 0, 0); //put window
+		wrefresh(win);
+		start_color();  //start color
 		t = time(NULL);
-		printf("\nCurrent Time: %s\n", ctime(&t));
 
+		init_pair(1, COLOR_GREEN, COLOR_BLACK); //initializing color
+		attron(COLOR_PAIR(1));
+		attron(A_BOLD);
+		move(2, 65);
+		printw("|| TEXT-BASED SHELL UTILITY ||");
+		attroff(A_BOLD);
+		attroff(COLOR_PAIR(1));
+
+		mvwprintw(win, 4, 1, "Current Time: %s", ctime(&t));
 		getcwd(s, 200);
-		printf("Current Working Directory: %s \n\n", s);
+		mvwprintw(win, 5, 1, "Current Working Directory: %s", s);
 
-		printf("\nFiles:");
+		mvwprintw(win, 9, 1, "Files:");
 		d = opendir(".");
 		c = 0;
+		int y = 10, x = 5;
 		while ((de = readdir(d)))
 		{
 			if ((de->d_type) & DT_REG)
 			{
-				printf("\t\t(%d File: %s)\n", c++, de->d_name);
+				if (stat(de->d_name, &statbuffer) == -1)
+        			continue;
+				mvwprintw(win, y++, x, "(%d File: %s)\n", c++, de->d_name);
+				mvwprintw(win,y-1,50,"%d bytes", (intmax_t)statbuffer.st_size);
 			}
 			/*if ((c % 8) == 0)
 			{
@@ -69,86 +94,104 @@ int main(void)
 			}*/
 		}
 		closedir(d);
-
-		printf("\nDirectories: ");
+		y++;
+		mvwprintw(win, y++, 1, "Directories: ");
 		d = opendir(".");
 		c = 0;
 		de = readdir(d);
+
 		if ((de->d_type) & DT_DIR)
-			printf("\t(%d Directory: %s)\n", c++, de->d_name);
+			mvwprintw(win, y++, x, "(%d Directory: %s)\n", c++, de->d_name);
 		while ((de = readdir(d)))
 		{
 			if ((de->d_type) & DT_DIR)
-				printf("\t\t(%d Directory: %s)\n", c++, de->d_name);
+			{
+				if (stat(de->d_name, &statbuffer) == -1)
+        			continue;
+				mvwprintw(win,y++, x, "(%d Directory: %s)", c++, de->d_name);
+				mvwprintw(win,y-1,50,"%d bytes", (intmax_t)statbuffer.st_size);
+			}
 		}
 		closedir(d);
 
-		printf("\nOperations:\tD Display\n\t\tE Edit\n\t\tR Run\n\t\tC Change Directory\n\t\tS Sort Directory Listing\n\t\tM Move to Directory\n\t\tR Remove File\n\t\tQ Quit\n\n");
-		
-		c = getchar();
-		if(c=='\n'){c = getchar();}
-		getchar();
-		switch (c)
+		y++;
+		mvwprintw(win, y++, 1, "Operations:");
+		mvwprintw(win, y++, x, "D Display");
+		mvwprintw(win, y++, x, "E Edit");
+		mvwprintw(win, y++, x, "R RUN");
+		mvwprintw(win, y++, x, "C Change Directory");
+		mvwprintw(win, y++, x, "S Sort Directory");
+		mvwprintw(win, y++, x, "M Move Directory");
+		mvwprintw(win, y++, x, "R Remove Directory");
+		mvwprintw(win, y++, x, "Q Quit\n");
+
+		refresh();
+		wrefresh(win);
+		y+=2; x=2;
+		wmove(win,y,x);
+		char z;
+		wgetnstr(win, &z, 1);
+		x=2;
+		switch (z)
 		{
 		case 'q':
+			endwin();
+			system("clear");
 			exit(0);
 		case 'e':
-			printf("Edit what?:");
+			mvwprintw(win, y++, x, "Edit what?:");
 			scanf("%s", s);
 			strcpy(cmd, "pico ");
 			strcat(cmd, s);
 			system(cmd);
 			break;
 		case 'r':
-			printf("Run what?:");
+			mvwprintw(win, y++, x, "Run what?:");
 			scanf("%s", cmd);
 			system(cmd);
 			break;
 		case 'c':
-			printf("Change To?:");
-			scanf("%s", cmd);
+			mvwprintw(win, y++, x, "Change To?:");
+			wgetstr(win, cmd);
 			chdir(cmd);
 			break;
 		case 's':
-			printf("Sorted Directories:");
+			mvwprintw(win, y++, x, "Sorted Directories:");
 			d = opendir(".");
 			c = 0;
 			de = readdir(d);
 			if ((de->d_type) & DT_DIR)
 				c++;
-				
+
 			while ((de = readdir(d)))
 			{
 				if ((de->d_type) & DT_DIR)
 					c++;
-					
 			}
 			closedir(d);
-			
+
 			char tosort[c][256];
 			d = opendir(".");
 			c = 0;
 			de = readdir(d);
 			if ((de->d_type) & DT_DIR)
 			{
-				strcpy(tosort[c],de->d_name);
+				strcpy(tosort[c], de->d_name);
 				c++;
 			}
-				
+
 			while ((de = readdir(d)))
 			{
 				if ((de->d_type) & DT_DIR)
 				{
-					strcpy(tosort[c],de->d_name);
+					strcpy(tosort[c], de->d_name);
 					c++;
 				}
-					
 			}
 			closedir(d);
-			sort_array(c,tosort);
+			sort_array(c, tosort);
 			break;
 		}
 	}
+	return 0;
 }
-
-
